b2f387083179aa3f8c2cc5aae921c30a1c511713
tim <tim@glo.id.au>
2021-10-06 12:36:46 +1100

    extend POC to work with text string content

--- crypo-key-storage.js
+++ crypo-key-storage.js
@@ -1,8 +1,18 @@
 async function encryptDataSaveKey() {
-  var data = await makeData();
+  var data = await makeRandomData();
   console.log("generated data", data);
-  var keys = await makeKeys();
+  var keys = await makeEncryptionKeys();
   var encrypted = await encrypt(data, keys);
+  callOnStore((store) => {
+    store.put({ id: 1, keys: keys, encrypted: encrypted });
+  });
+}
+
+async function encryptCustomTextDataSaveKey(text) {
+  var keys = await makeEncryptionKeys();
+  var encoder = new TextEncoder();
+  const encoded = encoder.encode(text);
+  var encrypted = await encrypt(encoded, keys);
   callOnStore((store) => {
     store.put({ id: 1, keys: keys, encrypted: encrypted });
   });
@@ -16,8 +26,23 @@ function loadKeyDecryptData() {
         var keys = getData.result.keys;
         var encrypted = getData.result.encrypted;
         var data = await decrypt(encrypted, keys);
-        console.log("decrypted data", data);
-        res({ data, encrypted });
+        console.log("decrypted data:", data);
+        res(data);
+      };
+    });
+  });
+}
+
+function loadKeyDecryptTextData() {
+  return new Promise((res) => {
+    callOnStore((store) => {
+      var getData = store.get(1);
+      getData.onsuccess = async () => {
+        var keys = getData.result.keys;
+        var encrypted = getData.result.encrypted;
+        var data = await decryptText(encrypted, keys);
+        console.log("decrypted data:", data);
+        res(data);
       };
     });
   });
@@ -67,11 +92,11 @@ function callOnStore(fn_) {
 //   console.log("decrypted data", finalData);
 // }
 
-function makeData() {
+function makeRandomData() {
   return window.crypto.getRandomValues(new Uint8Array(16));
 }
 
-function makeKeys() {
+function makeEncryptionKeys() {
   return window.crypto.subtle.generateKey(
     {
       name: "RSA-OAEP",
@@ -79,7 +104,8 @@ function makeKeys() {
       publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
       hash: { name: "SHA-256" }, // can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
     },
-    false, // whether the key is extractable (i.e. can be used in exportKey)
+    // false, // whether the key is extractable (i.e. can be used in exportKey)
+    true,
     ["encrypt", "decrypt"] // must be ["encrypt", "decrypt"] or ["wrapKey", "unwrapKey"]
   );
 }
@@ -97,6 +123,20 @@ function encrypt(data, keys) {
 
 async function decrypt(data, keys) {
   return new Uint8Array(
+    await window.crypto.subtle.decrypt(
+      {
+        name: "RSA-OAEP",
+        // label: Uint8Array([...]) // optional
+      },
+      keys.privateKey, // from generateKey or importKey above
+      data // ArrayBuffer of the data
+    )
+  );
+}
+
+async function decryptText(data, keys) {
+  const decoder = new TextDecoder();
+  return decoder.decode(
     await window.crypto.subtle.decrypt(
       {
         name: "RSA-OAEP",
